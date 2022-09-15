223fd92ad07748f88e39f986b1b5270602e872cd
Sophia Kerber Shigueoka <sophia.shigueoka@gmail.com>
2022-09-15 14:57:45 -0300

    Pause Menu without buttons using ESC

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,21 +10,34 @@ public class GameManager : MonoBehaviour
     StarshipController startship;
     public Text velocityText;
     public Text altitudeText;
+
     private float velocityX = 0;
     private float velocityY = 0;
-    private float altitude = 0;
+
+    private bool isPaused = false;
 
     public GameObject PausePanel;
     
     public void Start()
     {
-        print("AAAAAAAAAAAAAAAAA MANAGER");
-        velocityText.text = "X: " + velocityX.ToString("0") + "; " + "Y: " + velocityY.ToString("0");
+        PausePanel.gameObject.SetActive(false);
+        velocityText.text = $"X: {velocityX.ToString("0")}; Y: {velocityY.ToString("0")}";
         altitudeText.text = GameObject.Find("Starship").GetComponent<StarshipController>().transform.position.y.ToString("0");
     }
 
     public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused) {
+                pause();
+            }
+            else { 
+                resume(); 
+            }
+            
+        }
+
         velocityX = GameObject.Find("Starship").GetComponent<StarshipController>().velocityX;
         velocityY = GameObject.Find("Starship").GetComponent<StarshipController>().velocityY;
 
@@ -36,13 +49,15 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 0;
         PausePanel.gameObject.SetActive(true);
+        isPaused = true;
     }
 
     public void resume()
     {
         Time.timeScale = 1;
         PausePanel.gameObject.SetActive(false);
-    }
+        isPaused = false;
+}
 
 
 }

ff4b26ff47dbca165c3ae3e06f30f90e8741ce01
Sophia Kerber Shigueoka <sophia.shigueoka@gmail.com>
2022-09-21 15:12:10 -0300

    Battery about to die; landing coordinates and landing dock

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,18 +10,27 @@ public class GameManager : MonoBehaviour
     StarshipController startship;
     public Text velocityText;
     public Text altitudeText;
+    public Text targetText;
 
     private float velocityX = 0;
     private float velocityY = 0;
 
+    private float targetX = 0;
+    private float targetY = 0;
+
     private bool isPaused = false;
 
     public GameObject PausePanel;
+    public GameObject LandingCoord;
     
     public void Start()
     {
+        
         PausePanel.gameObject.SetActive(false);
+        targetX = GameObject.Find("Landing Dock").transform.position.x;
+        targetY = GameObject.Find("Landing Dock").transform.position.y;
         velocityText.text = $"X: {velocityX.ToString("0")}; Y: {velocityY.ToString("0")}";
+        targetText.text = $"X: {targetX.ToString("0")}; Y: {targetY.ToString("0")}";
         altitudeText.text = GameObject.Find("Starship").GetComponent<StarshipController>().transform.position.y.ToString("0");
     }
 
